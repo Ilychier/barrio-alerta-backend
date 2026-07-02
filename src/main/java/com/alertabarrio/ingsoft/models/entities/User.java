121707f16f2a6b15205349ea370cc661454719fb
Ilychier <jherreraah93@gmail.com>
2026-07-02 18:00:07 -0500

    Link users and configs to barrios
    Adds `barrioId` to user save/response DTOs and persists the user-to-barrio relation. Also stores `Configuracion` against a `User`, resolves configs by user ID when needed, and returns the associated IDs in responses.

--- src/main/java/com/alertabarrio/ingsoft/models/entities/User.java
+++ src/main/java/com/alertabarrio/ingsoft/models/entities/User.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -32,5 +35,9 @@ public class User {
 
     @Column(name = "address", nullable = false, length = 255)
     private String address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "barrio_id", nullable = true)
+    private Barrio barrio;
 
 }
