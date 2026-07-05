fefbd5749e7495059fe30e75b12a4805b40ca8d2
Ilychier <jherreraah93@gmail.com>
2026-07-05 18:18:48 -0500

    Make alerta.categoria optional; remove UsuarioBarrio
    Remove UsuarioBarrio feature (controller, DTOs, entity, repository, service and impl). Make Alerta.categoria nullable and update AlertaService to handle null categoriaId. Relax AlertaSaveDTO validation (categoriaId optional). AlertaController: add CORS/rest import cleanup and rename save() -> create(). Increase Cuadrante.telefono_emergencia length to 100. Make User.barrio non-nullable at DB level. Minor refactor in UserController getById and import reordering/cleanup across affected files.

--- src/main/java/com/alertabarrio/ingsoft/repositories/UsuarioBarrioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alertabarrio.ingsoft.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.alertabarrio.ingsoft.models.entities.UsuarioBarrio;
-
-public interface UsuarioBarrioRepository extends JpaRepository<UsuarioBarrio, Long> {
-    boolean existsByUsuarioIdAndBarrioId(Long usuarioId, Long barrioId);
-}
